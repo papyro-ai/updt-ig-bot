b23984ed870a1f6d808cbd9bba9c21726d903335
Alvaro Neves <alvaro@192.168.0.19>
2026-03-27 08:56:45 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+import os
+
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+SHEET_URL = os.getenv("SHEET_URL")
+
+def send_post(image_url, caption):
+    url = f"https://api.telegram.org/bot8312725978:AAEdPQ1LqVEIf1TuVmNiZbaRsZvTTzGFARM/sendPhoto"
+    
+    img = requests.get(image_url).content
+    
+    requests.post(
+        url,
+        data={"chat_id": CHAT_ID, "caption": caption},
+        files={"photo": img}
+    )
+
+def main():
+    df = pd.read_csv(SHEET_URL)
+
+    now = datetime.now()
+    window_start = now - timedelta(minutes=1)
+
+    for _, row in df.iterrows():
+        post_time = datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M")
+
+        if window_start <= post_time <= now:
+            send_post(row["image_url"], row["caption"])
+            print("Enviado:", row["caption"])
+
+if __name__ == "__main__":
+    main()
